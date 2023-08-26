2f451d8801f8bcd36d50857eaa33404a0d99976d
saucepoint <saucepoint@protonmail.com>
2023-08-26 13:26:54 -0400

    versioning and dependency setup

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,5 +1,8 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.20;
+
+import {IFlashLoanSimpleReceiver} from "aave-v3-core/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
+import {ISwapRouter} from "v3-periphery/interfaces/ISwapRouter.sol";
 
 contract Counter {
     uint256 public number;
