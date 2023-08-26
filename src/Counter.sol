711c1ebf7bc85682162f14dbeec36900718ed821
saucepoint <saucepoint@protonmail.com>
2023-08-26 16:33:54 -0400

    working leverage up

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,32 +1,47 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
+import "forge-std/Test.sol";
 import {IPool} from "aave-v3-core/contracts/interfaces/IPool.sol";
 import {IFlashLoanSimpleReceiver} from "aave-v3-core/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
 import {IPoolAddressesProvider} from "aave-v3-core/contracts/interfaces/IPoolAddressesProvider.sol";
-import {ISwapRouter} from "v3-periphery/interfaces/ISwapRouter.sol";
+import {IV3SwapRouter} from "swap-router-contracts/interfaces/IV3SwapRouter.sol";
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 import {WETH} from "solmate/tokens/WETH.sol";
 import {ICometMinimal} from "./interfaces/ICometMinimal.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
-contract Counter is IFlashLoanSimpleReceiver {
+contract Counter is IFlashLoanSimpleReceiver, Test {
+    using FixedPointMathLib for uint256;
+
     IPool constant aave = IPool(0xA238Dd80C259a72e81d7e4664a9801593F98d1c5);
     ICometMinimal constant compound = ICometMinimal(0x46e6b214b524310239732D51387075E0e70970bf);
-    ISwapRouter constant router = ISwapRouter(0x2626664c2603336E57B271c5C0b26F421741e481);
+    IV3SwapRouter constant router = IV3SwapRouter(0x2626664c2603336E57B271c5C0b26F421741e481);
 
     IERC20 public cbETH = IERC20(0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22);
     WETH public weth = WETH(payable(0x4200000000000000000000000000000000000006));
 
     constructor() {
-        cbETH.approve(address(aave), type(uint256).max);
-        weth.approve(address(aave), type(uint256).max);
-
-        cbETH.approve(address(compound), type(uint256).max);
+        maxApprove();
     }
 
-    function up() public {
-        uint256 amount = 1 ether;
-        aave.flashLoanSimple(address(this), address(cbETH), amount, new bytes(0), 0);
+    function upETH(uint256 leverage, uint256 collateralFactor, uint256 cbETHPrice) external payable {
+        weth.deposit{value: msg.value}();
+        up(msg.value, leverage, collateralFactor, cbETHPrice);
+    }
+
+    function up(uint256 wethAmount, uint256 leverage, uint256 collateralFactor, uint256 cbETHPrice) public {
+        // target ETH exposure
+        uint256 wethAmountTotal = wethAmount.mulWadDown(leverage);
+
+        // target ETH exposure, in the form of cbETH tokens
+        uint256 cbETHAmount = wethAmountTotal.divWadDown(cbETHPrice);
+
+        // how much borrow according to a collateral factor / LTV
+        uint256 borrowAmount = wethAmountTotal.mulWadDown(collateralFactor);
+
+        bytes memory data = abi.encodePacked(borrowAmount);
+        aave.flashLoanSimple(address(this), address(cbETH), cbETHAmount, data, 0);
     }
 
     function executeOperation(address asset, uint256 amount, uint256 premium, address initiator, bytes calldata params)
@@ -34,6 +49,7 @@ contract Counter is IFlashLoanSimpleReceiver {
         override
         returns (bool)
     {
+        require(initiator == address(this), "Counter: initiator not self");
         require(asset == address(cbETH) || asset == address(weth), "Counter: unknown asset");
 
         // flash borrowed cbETH: leveraging up
@@ -42,11 +58,24 @@ contract Counter is IFlashLoanSimpleReceiver {
             compound.supply(address(cbETH), amount);
 
             // borrow eth from compound
-            compound.withdraw(address(weth), amount / 3);
+            compound.withdraw(address(weth), abi.decode(params, (uint256)));
 
-            // swap WETH to cbETH
-
-            // repay flashloan
+            // swap WETH to cbETH to repay flashloan
+            uint256 repayAmount;
+            unchecked {
+                repayAmount = amount + premium;
+            }
+            router.exactOutputSingle(
+                IV3SwapRouter.ExactOutputSingleParams({
+                    tokenIn: address(weth),
+                    tokenOut: address(cbETH),
+                    fee: 500,
+                    recipient: address(this),
+                    amountOut: amount + premium,
+                    amountInMaximum: type(uint256).max, // TODO: set max slippage
+                    sqrtPriceLimitX96: 0
+                })
+            );
         } else { // flash borrow WETH: deleveraging
                 // repay on compound
                 // withdraw cbETH
@@ -62,5 +91,14 @@ contract Counter is IFlashLoanSimpleReceiver {
 
     function POOL() external pure override returns (IPool) {
         return aave;
+    }
+
+    function maxApprove() public {
+        cbETH.approve(address(aave), type(uint256).max);
+        weth.approve(address(aave), type(uint256).max);
+
+        cbETH.approve(address(compound), type(uint256).max);
+
+        weth.approve(address(router), type(uint256).max);
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -7,13 +7,20 @@ import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+    IERC20 cbETH;
+
+    address alice = makeAddr("alice");
 
     function setUp() public {
         counter = new Counter();
-        deal(address(counter.cbETH()), address(counter), 10 ether);
+        cbETH = IERC20(address(counter.cbETH()));
+        deal(alice, 10 ether);
+        deal(address(cbETH), address(counter), 1 wei); // dust
     }
 
     function test_up() public {
-        counter.up();
+        vm.startPrank(alice);
+        counter.upETH{value: 1 ether}(4e18, 0.8e18, 1.047e18);
+        assertEq(cbETH.balanceOf(address(counter)), 1 wei);
     }
 }
